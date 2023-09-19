487e3b461083959d81b9ebc8a207dd26497cbbc4
WillyWilliam00 <costawilliam98@gmail.com>
2023-09-19 23:31:21 +0200

    up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Row = document.querySelector(".row")
 const SearchValue = document.querySelector("input[type=search]")
 const NextButton = document.querySelector(".next")
 const pagenumber = document.querySelector(".page")
-const main = document.querySelector("main")
 const footer = document.querySelector("footer")
 let count = 1
 
@@ -15,18 +14,19 @@ function FirstFetch(object) {
         .then(body => {
             const photos = body.photos
             if (photos.length !== 0) {
-                const ArrayCards = photos.map(photo =>
-                /*html*/ `<div class="col-3 mb-4">
+                const ArrayCards = photos.map(photo => /*html*/
+                 `<div class="col-3 mb-4">
                                 <div class="card"><img src="${photo.src.tiny}" class="card-img-top">
                                     <div class="card-body">
                                         <h5 class="card-title">Photographer: ${photo.photographer}</h5>
                                         <p class="card-text">${photo.alt}</p>
                                     </div>
                                 </div>
-                        </div>`
+                            </div>`
                 )
                 Row.innerHTML = ArrayCards.join("")
-                pagenumber.innerText = `Page: ${count}`
+                pagenumber.innerHTML = /*html*/
+                 `Page: <span style="color: red;">${count}</span> | Quantity: <span style="color: red;">${count * 15}</span>`
                 footer.classList.remove("d-none")
             } else {
                 footer.classList.add("d-none")
@@ -45,31 +45,25 @@ function next() {
         .then(response => response.json())
         .then(body => {
             const photos = body.photos
-            const ArrayCards = photos.map(photo =>
-        /*html*/ `<div class="col-3 mb-4">
+            const ArrayCards = photos.map(photo => /*html*/
+                    `<div class="col-3 mb-4">
                         <div class="card"><img src="${photo.src.tiny}" class="card-img-top">
                             <div class="card-body">
                                 <h5 class="card-title">Photographer: ${photo.photographer}</h5>
                                 <p class="card-text">${photo.alt}</p>
                             </div>
                         </div>
-                </div>`
+                    </div>`
             )
-            pagenumber.innerText = `Page: ${count}`
-            let NextCont = document.createElement("div");
-            NextCont.classList.add("container")
-            NextCont.classList.add("mt-5")
-            let NextRow = document.createElement("div");
-            NextRow.classList.add("row")
-            NextCont.appendChild(NextRow)
-            main.appendChild(NextCont)
-            NextRow.innerHTML = ArrayCards.join("")
+            pagenumber.innerHTML = /*html*/
+             `Page: <span style="color: red;">${count}</span> | Quantity: <span style="color: red;">${count * 15}</span>`
+            Row.innerHTML += ArrayCards.join("")
         })
 
 }
 
 function search() {
-    main.innerHTML = ""
+
     if (SearchValue.value) {
         FirstFetch(SearchValue.value)
     } else {
